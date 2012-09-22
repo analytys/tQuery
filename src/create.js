c2e4a55c5935beedbb3a76994069353dcae1c531
tocky <toc.ky@foxmail.com>
2012-09-22 17:05:40 +0800

    ...

--- src/create.js
+++ src/create.js
@@ -36,60 +36,6 @@
  * @returns {Function}
  */
 
-var tQueryid  ;
-
-var tQuery_list ; 
-var tag_list ;
-var id_list ;
-var cls_list ;
-
-var current_page ; // 设置当前处理的page
-
-
-// 略微特殊一点
-function pushtQueryList(element,parent)
-{
-//	pushList(tQuery_list,element,parent);
-}
-
-function pushTagList(tag,tid)
-{
-	pushList(tQuery_list,tag ,tid);
-}
-
-function pushClsList(cls,tid)
-{
-	// 一个元素可能有多个cls
-	// 将cls切分
-	var arr = cls.split("") ;
-	for(i=0,len<arr.length;i<len;i++)
-	{
-		pushList(tQuery_list, trim(arr[i]) ,tid);
-	}
-}
-
-function pushidList(id,tid)
-{
-	pushList(tQuery_list,id , tid);
-}
-
-function pushList(list,type,tid)
-{
-	list.type = tid ;
-}
-
-function gettQueryid()
-{
-	var tQueryid = tQueryid || parseInt(Math.random() * (1000000 - 100 + 1) + 100) ;
-	return tQueryid + 1 ;
-};
-
-/*****for test *****/
-function trim(str)
-{
-	return str ;
-}
-
 var tQuery = {
 		type : function(ele)
 		{
@@ -101,92 +47,264 @@ var tQuery = {
 			return true ;
 		}
 };
-/****** for test *****/
 
-function addElementToUIChain(element)
-{
-	element = tQuery.type(element) === "object" ? element : {};
-	
-	if( element.has )
-};
 
-function handleChildren(children)
+/**
+ * 创建UI 对象
+ * like  :
+ * 
+ * {
+ * 		type:"window",
+ * 		title : "Hello tQuery",
+ * 		color : "#fff000",
+ * 		background-image : "img/bk.png" ,
+ * 		children : [
+ * 					{
+ * 						type : "label",
+ * 						id : "label_register" ,
+ * 						cls : "normal",
+ * 						style : "",
+ * 					},
+ * 					{
+ * 						type : "view",
+ * 						children : [
+ * 										{
+ * 											type : "TextField",
+ * 											hint : L("put your name") ,
+ * 										}
+ * 									] ,
+ * 					},
+ * 					{
+ * 						type : "button",
+ * 						click : function(){},
+ * 						event : {
+ * 									dblclick : function(e){},
+ * 									click : function(e){}
+ * 								}
+ * 					},
+ * 					]
+ * 
+ * }
+ * 
+ * 内部存储：
+ * 
+ * 每一个element 唯一绑定一个tQueryid
+ * 
+ * tQueryid list
+ * {
+ * 		aaaaaa : {
+ * 					parent : tQueryid ,
+ * 					children : [tQueryid ,tQueryid,tQueryid ],
+ * 					opt : {},
+ * 					...........
+ * 				}
+ * }
+ * 
+ * 根据id , cls , tag 
+ * 
+ * {
+ * 		a : tQueryid , 
+ * 		b : tQueryid , 
+ * }
+ * 
+ * @param {Object} obj 布局page对象
+ */
+var create = function(opt)
 {
-	children = children ? children : [] ; // 判断是否数组
-	for(var i=0,len=children.length;i<len;i++)
+	return (function(tQuery)
 	{
-		handleUI(children[i]);
-	}
-}
-
-function handleUI( obj , parent )
-{
-		var types = ["page","window","view","button"] ;
-
-		// handle type 
-		if( !obj.hasOwnProperty("type") )
-		{
-			// log error
-			return ;
-		}
-		
-		// Unrecognized type
-		if( !tQuery.inArray(obj.type,types) )
-		{
-			// log error
-			return ;
-		}
-		
-		var tQueryid = gettQueryid(); // tQuery 有guid 方法，只要保证唯一即可
-		
-		// 添加到 tQueryid 列表
-		pushtQueryidList( obj , parent , tQueryid);
-		
-		// 添加到tag列表
-		pushTagList(obj.type );
-		
-		// 添加到id列表
-		if( obj.hasOwnProperty("id") )
-		{
-			pushidList(obj.id , tQueryid );
-		}
-		
-		// 添加到cls列表
-		if( obj.hasOwnProperty("cls") )
-		{
-			pushClsList(obj.cls , tQueryid );
-		}
-
-		if( obj.hasOwnProperty("className") )
-		{
-			pushClsList(obj.className , tQueryid);
-		}
-		
-		/* 处理子element */
-		if( obj.hasOwnProperty("children") )
-		{
-			handleChildren(obj.children);
-		}
-};
-
-
-function create(opt)
-{
-	opt = tQuery.type(opt) === "object" ? opt : {} ; 
+		var tQueryid  = parseInt(Math.random() * (10000 - 100 + 1) + 100) ;
 	
-	// 默认给opt添加一个page父对象
-	if( !opt.hasOwnPropertype("type") || opt.type != "page" )
-	{
-		opt = {
-				type: "page" ,
-				children : [ opt ] , // children 数组
+		/* 只保留tQueryid list */
+		var tag_list = {} ;
+		var id_list = {};
+		var cls_list = {};
+		var tQuery_list = {} ;
+		
+		// 略微特殊一点
+		// 
+		//{
+		//	tQueryid : {
+		//		parent : tQueryid ,
+		//		children : [tQueryid,tQueryid]
+		//		opt : {}  // copy from opt , non children
+			
+		//	}
+		//}
+		function pushtQueryidList(tQueryid , obj , parent)
+		{
+			var opt = {} ; // new copy
+			for(var i in obj )
+			{
+				opt[i] = obj[i] ;
+			}
+			
+			if(opt['children'])
+			{
+				delete opt.children ;
+			}
+			
+			tQuery_list[tQueryid]  = tQuery_list[tQueryid] || {} ;
+			tQuery_list[tQueryid]['parent'] = parent || "" ;
+			tQuery_list[tQueryid]['opt'] = opt ;
+			tQuery_list[tQueryid]['children'] =  tQuery_list[tQueryid]['children'] || [] ;
+			
+			if( tQuery_list[parent]) // 存在说明则更新父节点的子节点值
+			{
+				tQuery_list[parent]['children'].push( tQueryid ) ;
+			}
+		}
+		
+		
+		function pushTagList(tag,tid)
+		{
+			tag_list[tag] = tag_list[tag] || [] ;
+			tag_list[tag].push( tid );
+		}
+		
+		function pushClsList(cls,tid)
+		{
+			// 一个元素可能有多个cls
+			// 将cls切分
+			var arr = cls.split("") ;
+			for(var  i = 0,len=arr.length;i<len;i++)
+			{
+				cls_list[cls] = cls_list[cls] || [] ;
+				cls_list[cls].push(tid);
+			}
+		}
+		
+		function pushidList(id,tid)
+		{
+			id_list[id] = cls_list[id] || [] ;
+			id_list[id].push(tid);
+		}
+		
+		function gettQueryid()
+		{
+			return ++tQueryid ;
 		};
-	}
-	
-	
-	
-	return function()
-	{
-		return handleUI(page) ;
-	} ;
-}
+		
+		/*****for test *****/
+		function trim(str)
+		{
+			return str ;
+		}
+		
+		function L(str)
+		{
+			return str ;
+		}
+		/****** for test end *****/
+		
+		function handleChildren(children , parent )
+		{
+			children = children ? children : [] ; // 判断是否数组
+			for(var i=0,len=children.length;i<len;i++)
+			{
+				handleUI(children[i] , parent );
+			}
+		}
+		
+		function handleUI( obj , parent )
+		{
+				var types = ["page","window","view","button"] ;
+		
+				// handle type 
+				if( !obj["type"] )
+				{
+					// log error
+					return ;
+				}
+				
+				// Unrecognized type
+				if( !tQuery.inArray(obj.type.toLowerCase(),types) )
+				{
+					// log error
+					return ;
+				}
+				
+				var tQueryid = gettQueryid(); // 当前对象的唯一标识符
+				
+				// 添加到 tQueryid 列表
+				pushtQueryidList( tQueryid , obj , parent );
+				
+				// 添加到tag列表
+				pushTagList(obj.type.toLowerCase() , tQueryid );
+				
+				// 添加到id列表
+				if( obj["id"] )
+				{
+					pushidList(obj.id.toLowerCase() , tQueryid );
+				}
+				
+				// 添加到cls列表
+				if( obj["cls"] )
+				{
+					pushClsList(obj.cls.toLowerCase() , tQueryid );
+				}
+		
+				if( obj["className"] )
+				{
+					pushClsList(obj.className.toLowerCase() , tQueryid);
+				}
+		
+				if( obj["class"] )
+				{
+					pushClsList(obj['class'].toLowerCase() , tQueryid);
+				}
+				
+				/* 处理子element */
+				if( obj["children"] )
+				{
+					handleChildren(obj.children , tQueryid );
+				}
+		};
+		
+		
+		var create = function(opt)
+		{
+			opt = (opt && tQuery.type(opt) === "object") ? opt : {} ; 
+			
+			// 默认给opt添加一个page父对象
+			if( !opt["type"] || opt.type != "page" )
+			{
+				opt = {
+						type: "page" ,
+						children : [ opt ] , // children 数组
+				};
+			}
+			
+			return handleUI(opt);
+		}
+		
+		return create(opt) ; // return function
+		
+	})(tQuery,opt);
+
+
+// UIchain 对象用于扩展tQuery
+// UIchain = {
+//	"id":{},
+//	"cls":{},
+//	"tag":{},
+//	"tlist":{},
+//}
+var UIchain = (function() {
+	// 创建一个隐藏的object, 这个object持有一些数据
+	// 从外部是不能访问这个object的
+	var data = {};
+	// 创建一个函数, 这个函数提供一些访问data的数据的方法
+	return function(key, val) {
+	    if (val === undefined)
+	    {
+	    	return data[key] ;
+	    } // get
+	    else
+	    { 
+	    	return data[key] = val ;
+	    } // set
+	};
+	// 我们可以调用这个匿名方法
+	// 返回这个内部函数，它是一个闭包
+})();
